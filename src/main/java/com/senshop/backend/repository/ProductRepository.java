e740323b05b71140591abde5f19b7bfeeab9cbe8
WindyWin <75726620+WindyWin@users.noreply.github.com>
2022-06-14 00:39:23 +0700

    blog 1/4

--- src/main/java/com/senshop/backend/repository/ProductRepository.java
+++ src/main/java/com/senshop/backend/repository/ProductRepository.java
@@ -2,13 +2,9 @@ package com.senshop.backend.repository;
 
 import com.senshop.backend.model.*;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+
 
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.mongodb.repository.Update;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.stereotype.Repository;
 
